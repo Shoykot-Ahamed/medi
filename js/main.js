f4ba600aa19304b71e3ece5bac645b3bb4db0a6b
Shoykot-Ahamed <shoykotahamed7736@gmail.com>
2020-02-29 17:57:23 +0600

    first page

--- js/main.js
+++ js/main.js
@@ -150,4 +150,65 @@ function mainSlider() {
 }
 mainSlider();
 
+$(function(){
+	$('.datepicker').datepicker()
+});
+
+  // brand-active
+  $('.client-active').slick({
+	dots: true,
+	arrows: false,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 5,
+	slidesToScroll: 2,
+	responsive: [
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  infinite: true,
+		  dots: false
+		}
+	  },
+	  {
+		breakpoint: 991,
+		settings: {
+		  slidesToShow: 4,
+		  slidesToScroll: 1
+		}
+	  },
+	  {
+		breakpoint: 767,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+
+	]
+  });
+
+  // testimonial-active
+  $('.testimonial-active').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: true,
+	dots:true,
+	asNavFor: '.testimonial-nav'
+  });
+  $('.testimonial-nav').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	asNavFor: '.testimonial-item-active',
+	dots: true,
+	prevArrow:'<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+	nextArrow:'<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+	centerMode: true,
+	focusOnSelect: true,
+	centerPadding:0
+  });
+
 })(jQuery);
