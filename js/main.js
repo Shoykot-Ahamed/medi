02fce3bfe20fa8c07a976dd112ea5b825019ef55
Shoykot-Ahamed <shoykotahamed7736@gmail.com>
2020-03-01 14:02:32 +0600

    first page

--- js/main.js
+++ js/main.js
@@ -211,4 +211,18 @@ $(function(){
 	centerPadding:0
   });
 
+// map
+
+function initMap() {
+	var location = {lat: -25.363, lng: 131.044};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 8,
+		center: location,
+	});
+	var marker = new google.maps.marker({
+		position: location,
+		map: map,
+	});
+}
+
 })(jQuery);
