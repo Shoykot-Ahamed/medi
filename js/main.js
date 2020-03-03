70a273e94c4ff616735470d87b0702bd93c28af0
Shoykot-Ahamed <shoykotahamed7736@gmail.com>
2020-03-04 05:13:44 +0600

    blog

--- js/main.js
+++ js/main.js
@@ -199,20 +199,47 @@ $(function(){
 	arrows: false,
 	fade: true,
 	dots:true,
-	asNavFor: '.testimonial-nav'
-  });
-  $('.testimonial-nav').slick({
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	dots: true,
-	prevArrow:'<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
-	nextArrow:'<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
-
-	centerPadding:0
   });
 
   // team - active
   $('.team-active').slick({
+	dots: false,
+	arrows: true,
+	infinite: true,
+	prevArrow: '.arrow-prev',
+	nextArrow: '.arrow-next',
+	speed: 300,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	responsive: [
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  infinite: true,
+		  dots: false
+		}
+	  },
+	  {
+		breakpoint: 991,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 1
+		}
+	  },
+	  {
+		breakpoint: 767,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+
+	]
+  });
+  
+  $('.team-active-2').slick({
 	dots: false,
 	arrows: true,
 	infinite: true,
