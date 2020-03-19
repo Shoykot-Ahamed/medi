74a07f9e0ae0b65b02ad83834308765bb0525029
Shoykot-Ahamed <shoykotahamed7736@gmail.com>
2020-03-19 15:06:24 +0600

    pricing table solve

--- js/main.js
+++ js/main.js
@@ -290,34 +290,34 @@ $(function(){
 
   // pricing-active
 
-	var e = document.getElementById("filt-monthly"),
-	d = document.getElementById("filt-hourly"),
-	t = document.getElementById("switcher"),
-	m = document.getElementById("monthly"),
-	y = document.getElementById("hourly");
+var e = document.getElementById("filt-monthly"),
+d = document.getElementById("filt-hourly"),
+t = document.getElementById("switcher"),
+m = document.getElementById("yearly"),
+y = document.getElementById("monthly");
 
-	e.addEventListener("click", function(){
-	t.checked = false;
-	e.classList.add("toggler--is-active");
-	d.classList.remove("toggler--is-active");
-	m.classList.remove("hide");
-	y.classList.add("hide");
-	});
+e.addEventListener("click", function(){
+t.checked = false;
+e.classList.add("toggler--is-active");
+d.classList.remove("toggler--is-active");
+m.classList.remove("hide");
+y.classList.add("hide");
+});
 
-	d.addEventListener("click", function(){
-	t.checked = true;
-	d.classList.add("toggler--is-active");
-	e.classList.remove("toggler--is-active");
-	m.classList.add("hide");
-	y.classList.remove("hide");
-	});
+d.addEventListener("click", function(){
+t.checked = true;
+d.classList.add("toggler--is-active");
+e.classList.remove("toggler--is-active");
+m.classList.add("hide");
+y.classList.remove("hide");
+});
 
-	t.addEventListener("click", function(){
-	d.classList.toggle("toggler--is-active");
-	e.classList.toggle("toggler--is-active");
-	m.classList.toggle("hide");
-	y.classList.toggle("hide");
-	})
+t.addEventListener("click", function(){
+d.classList.toggle("toggler--is-active");
+e.classList.toggle("toggler--is-active");
+m.classList.toggle("hide");
+y.classList.toggle("hide");
+})
 
 
 })(jQuery);
